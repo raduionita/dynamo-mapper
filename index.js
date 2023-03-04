76e50c4c0dc5a95870b5d0773edd946a3b518c41
Radu Ionita <ionita.radu.ionut@gmail.com>
2023-03-04 20:34:48 +0200

    multiple ORs

--- index.js
+++ index.js
@@ -1,70 +1,82 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.resolver = exports.Resolver = void 0;
 class Resolver {
-    constructor() {
+    constructor(trier) {
         this._trier = { call: null, thens: [] };
-        this._elser = { call: null, thens: [] };
+        this._orers = [];
+        if (trier) {
+            this._trier.call = trier;
+        }
     }
     try(trier) {
         this._trier.call = trier;
         return this;
     }
     then(call) {
-        if (!this._elser.call) {
+        if (this._orers.length === 0) {
             this._trier.thens.push(call);
         }
         else {
-            this._elser.thens.push(call);
+            this._orers[this._orers.length - 1].thens.push(call);
         }
         return this;
     }
     or(call) {
-        this._elser.call = call;
+        this._orers.push({ call: call, thens: [] });
         return this;
     }
     exec() {
         return new Promise(async (rez, rej) => {
-            console.log('exec', 'Promise');
             const data = await (this._trier.call());
             if (data) {
-                console.log('exec', 'Promise', 'try');
                 let rezl = data;
-                console.log('exec', 'Promise', 'try', 'before thens', rezl);
-                for (const then of this._trier.thens)
-                    rezl = then(rezl);
-                console.log('exec', 'Promise', 'try', 'after thens', rezl);
+                let comp = null;
+                for (const then of this._trier.thens) {
+                    comp = then(rezl);
+                    if (comp instanceof Promise)
+                        comp = await comp;
+                    if (comp && typeof comp === typeof rezl)
+                        rezl = comp;
+                }
                 return rez(rezl);
             }
-            else if (this._elser.call) {
-                console.log('exec', 'Promise', 'or');
-                const data = await (this._elser.call());
-                if (data) {
-                    let relz = data;
-                    let comp = null;
-                    for (const then of this._elser.thens) {
-                        comp = then(relz);
-                        if (comp instanceof Promise)
-                            comp = await comp;
-                        if (comp)
-                            relz = comp;
+            else if (this._orers.length !== 0) {
+                for (const _orer of this._orers) {
+                    const data = await (_orer.call());
+                    if (data) {
+                        let rezl = data;
+                        let comp = null;
+                        for (const then of _orer.thens) {
+                            comp = then(rezl);
+                            if (comp instanceof Promise)
+                                comp = await comp;
+                            if (comp && typeof comp === typeof rezl)
+                                rezl = comp;
+                        }
+                        return rez(rezl);
                     }
-                    return rez(relz);
                 }
             }
             return rez(null);
         });
     }
 }
+exports.Resolver = Resolver;
 ;
-function resolver() {
-    return new Resolver;
+function resolver(trier) {
+    return new Resolver(trier);
 }
+exports.resolver = resolver;
 (async () => {
     const data = await resolver().try(() => new Promise((rez, rej) => rez(null)))
-        .then((data) => (Object.assign({ 'then': 1 }, data)))
-        .or(() => new Promise((rez, rej) => rez({ 'or': 2 })))
-        .then((data) => (Object.assign({ 'then': 1 }, data)))
-        .then((data) => new Promise((rez, rej) => rez(data)))
+        .then((data) => (Object.assign({ 'then': '1.1' }, data)))
+        .or(() => new Promise((rez, rej) => rez(null)))
+        .then((data) => (Object.assign({ 'then': '2.1' }, data)))
+        .then((data) => new Promise((rez, rej) => rez(Object.assign(Object.assign({}, data), { then: '2.2' }))))
+        .then((data) => new Promise((rez, rej) => rez(1)))
+        .or(() => new Promise((rez, rej) => rez({ 'or': 3 })))
+        .then((data) => new Promise((rez, rej) => rez(Object.assign(Object.assign({}, data), { 'then': 5 }))))
         .exec();
     console.log(data);
 })();
--- index.ts
+++ index.ts
@@ -1,60 +1,72 @@
-import { type } from "os";
-
+type Call = ()=>Promise<unknown>;
 type Then = (data:unknown)=>unknown;
-type Path = {call:()=>Promise<unknown>, thens:Then[]};
+type Path = {call?:Call, thens:Then[]};
 
-class Resolver {
-  private _trier:Path = {call:null, thens:[]} as Path;
-  private _elser:Path = {call:null, thens:[]} as Path;
+export class Resolver<T = unknown> {
+  private _trier:Path   = {call:null, thens:[]} as Path;
+  private _orers:Path[] = [];
 
+  constructor(trier?:Call) {
+    if (trier) {
+      this._trier.call = trier;
+    }
+  }
 
-  try(trier:()=>Promise<unknown>) : Resolver {
+  try(trier:Call) : Resolver<T> {
     this._trier.call = trier;
     return this;
   }
 
-  then(call:(data:unknown)=>unknown) : Resolver {
-    if (!this._elser.call) {
+  then(call:(data:unknown)=>unknown) : Resolver<T> {
+    if (this._orers.length === 0) {
       // or/elser has not been set, add to _trier
       this._trier.thens.push(call);
     } else {
       // or/elser has beens set, add to _elser
-      this._elser.thens.push(call);
+      this._orers[this._orers.length-1].thens.push(call);
     }
     return this;
   }
 
-  or(call:()=>Promise<unknown>) : Resolver {
-    this._elser.call = call;
+  or(call:Call) : Resolver<T> {
+    this._orers.push({call:call, thens:[]});
     return this;
   }
 
-  exec() : Promise<unknown> {
+  exec() : Promise<T> {
     return new Promise(async (rez, rej) => {
-      console.log('exec', 'Promise')
       const data = await (this._trier.call());
       if (data) {
-        console.log('exec', 'Promise', 'try');
         let rezl = data;
+        let comp = null;
         // exec all .then(s) adjacent to .try
-        for (const then of this._trier.thens)
-          rezl = then(rezl);
+        for (const then of this._trier.thens) {
+          comp = then(rezl);
+          if (comp instanceof Promise)
+            comp = await comp;
+          if (comp && typeof comp === typeof rezl)
+           rezl = comp;
+        }
         // done
-        return rez(rezl);
-      } else if (this._elser.call) {
-        console.log('exec', 'Promise', 'or')
-        const data = await (this._elser.call());
-        if (data) {
-          let relz = data;
-          let comp = null;
-          for (const then of this._elser.thens) {
-            comp = then(relz);
-            if (comp instanceof Promise)
-              comp = await comp;
-            if (comp)
-              relz = comp;
+        return rez(rezl as T);
+      } else if (this._orers.length !== 0) {
+        for (const _orer of this._orers) { 
+          const data = await (_orer.call());
+          // if data => continue w/ thens
+          if (data) {
+            let rezl = data;
+            let comp = null;
+            for (const then of _orer.thens) {
+              comp = then(rezl);
+              if (comp instanceof Promise)
+                comp = await comp;
+              if (comp && typeof comp === typeof rezl)
+                rezl = comp;
+            }
+            // return, skipping the rest of the orers
+            return rez(rezl as T);
           }
-          return rez(relz);
+          // else => continue w/ next or
         }
       }
       return rez(null);
@@ -63,16 +75,19 @@ class Resolver {
 };
 
 
-function resolver() : Resolver {
-  return new Resolver;
+export function resolver<T = unknown>(trier?:Call) : Resolver<T> {
+  return new Resolver(trier);
 }
 
 (async () => {
   const data = await resolver().try(() => new Promise((rez,rej) => rez(null))) // if found => return
-                                 .then((data:{}) => ({'then':1, ...data}))
-                               .or(() => new Promise((rez,rej) => rez({'or':2}))) // if not found return
-                                 .then((data:{}) => ({'then':1, ...data}))
-                                 .then((data:{}) => new Promise((rez,rej) => rez(data))) // 
+                                 .then((data:{}) => ({'then':'1.1', ...data}))
+                               .or(() => new Promise((rez,rej) => rez(null))) // if not found return
+                                 .then((data:{}) => ({'then':'2.1', ...data}))
+                                 .then((data:{}) => new Promise((rez,rej) => rez({...data, then:'2.2'})))
+                                 .then((data:{}) => new Promise((rez,rej) => rez(1)))
+                               .or(() => new Promise((rez,rej) => rez({'or':3})))
+                                 .then((data:{}) => new Promise((rez,rej) => rez({...data,'then':5})))
                                .exec();
   console.log(data);
 })();
